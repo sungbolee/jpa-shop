a8c47a7ca97b347c0430abeca1bcb1cbb4647c26
sungbolee <sungbobackend@gmail.com>
2022-04-10 12:48:53 +0900

    feat: Order 로직 추가

--- src/main/java/study/jpashop/domain/order/Order.java
+++ src/main/java/study/jpashop/domain/order/Order.java
@@ -3,6 +3,8 @@ package study.jpashop.domain.order;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import study.jpashop.domain.code.DeliveryStatus;
 import study.jpashop.domain.code.OrderStatus;
 import study.jpashop.domain.delivery.Delivery;
 import study.jpashop.domain.orderitem.OrderItem;
@@ -19,7 +21,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders")
-@Getter
+@Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
@@ -52,5 +54,43 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(User user, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setUser(user);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
     }
 }
