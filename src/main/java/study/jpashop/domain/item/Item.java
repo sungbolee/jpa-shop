d0e5230a715b83acafcd0d448f14a081b713c676
sungbolee <sungbobackend@gmail.com>
2022-04-10 11:31:52 +0900

    feat: Item stock 로직 추가

--- src/main/java/study/jpashop/domain/item/Item.java
+++ src/main/java/study/jpashop/domain/item/Item.java
@@ -20,4 +20,23 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+//        if (restStock < 0) {
+//            throw new NotEnoughStockException("need more stock");
+//        }
+        this.stockQuantity = restStock;
+    }
 }
