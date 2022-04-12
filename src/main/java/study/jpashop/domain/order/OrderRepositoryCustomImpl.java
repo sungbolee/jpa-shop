205727ed512c77385aec201bd5563a100312a8b1
sungbolee <sungbobackend@gmail.com>
2022-04-13 01:23:30 +0900

    refactor: Querydsl 지원 클래스 추가

--- src/main/java/study/jpashop/domain/order/OrderRepositoryCustomImpl.java
+++ src/main/java/study/jpashop/domain/order/OrderRepositoryCustomImpl.java
@@ -1,51 +1,43 @@
 package study.jpashop.domain.order;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.jpashop.api.v1.order.OrderDto;
 import study.jpashop.api.v1.order.OrderSearchCondition;
 import study.jpashop.api.v1.order.QOrderDto;
+import study.jpashop.domain.Querydsl4RepositorySupport;
 import study.jpashop.domain.code.OrderStatus;
-
-import javax.persistence.EntityManager;
-
-import java.util.List;
 
 import static study.jpashop.domain.delivery.QDelivery.delivery;
 import static study.jpashop.domain.order.QOrder.order;
 import static study.jpashop.domain.user.QUser.user;
 
-public class OrderRepositoryCustomImpl implements OrderRepositoryCustom {
+public class OrderRepositoryCustomImpl extends Querydsl4RepositorySupport implements OrderRepositoryCustom {
 
-    private final JPAQueryFactory queryFactory;
-
-    public OrderRepositoryCustomImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
+    public OrderRepositoryCustomImpl() {
+        super(Order.class);
     }
 
     @Override
     public Page<OrderDto> search(OrderSearchCondition condition, Pageable pageable) {
-        List<OrderDto> content = queryFactory
+        return applyPagination(pageable, contentQuery -> contentQuery
                 .select(new QOrderDto(order))
                 .from(order)
                 .leftJoin(order.user, user).fetchJoin()
                 .leftJoin(order.delivery, delivery).fetchJoin()
                 .where(statusEq(condition.getOrderStatus()),
+                        nameLike(condition.getUserName())
+
+                ), countQuery -> countQuery
+                .select(order.id)
+                .from(order)
+                .leftJoin(order.user, user).fetchJoin()
+                .leftJoin(order.delivery, delivery).fetchJoin()
+                .where(statusEq(condition.getOrderStatus()),
                         nameLike(condition.getUserName()))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-
-        JPAQuery<Long> countQuery = queryFactory
-                .select(order.count())
-                .from(order);
-
-        return PageableExecutionUtils.getPage(content, pageable,countQuery::fetchOne);
+        );
     }
 
     private BooleanExpression statusEq(OrderStatus statusCond) {
