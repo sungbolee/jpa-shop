5755b3b85786b3728c90fbd7ca0da151c86a319a
sungbolee <sungbobackend@gmail.com>
2022-04-14 16:26:26 +0900

    feat: LogInterceptor 추가

--- src/main/java/study/jpashop/config/WebConfig.java
+++ src/main/java/study/jpashop/config/WebConfig.java
@@ -3,6 +3,7 @@ package study.jpashop.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import study.jpashop.web.interceptor.LogInterceptor;
 import study.jpashop.web.interceptor.LoginCheckInterceptor;
 
 @Configuration
@@ -10,8 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
+        registry.addInterceptor(new LogInterceptor())
                 .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/users/add", "/login", "/logout",
                         "/css/**", "/*.ico", "/error");
