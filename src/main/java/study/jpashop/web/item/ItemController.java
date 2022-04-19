c5c0336ce3e10f29f07671ecd7057ba639dff331
sungbolee <sungbobackend@gmail.com>
2022-04-19 16:47:14 +0900

    feat: 파일 다운로드 기능 추가

--- src/main/java/study/jpashop/web/item/ItemController.java
+++ src/main/java/study/jpashop/web/item/ItemController.java
@@ -2,10 +2,15 @@ package study.jpashop.web.item;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriUtils;
 import study.jpashop.api.v1.item.ItemDto;
 import study.jpashop.domain.FileStore;
 import study.jpashop.domain.UploadFile;
@@ -16,6 +21,8 @@ import study.jpashop.service.item.ItemService;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -94,5 +101,29 @@ public class ItemController {
     public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
         itemService.updateItem(form.getId(), form.getName(), form.getPrice());
         return "redirect:/items";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.getById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
     }
 }
